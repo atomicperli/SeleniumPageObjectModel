fe932e7fc07b5b840fe9a1cb4f49fbe8b56249fa
yperli <54550399+yperli@users.noreply.github.com>
2022-02-20 17:22:19 +0530

    Update TestingClass.java

--- src/test/java/GitPackage/TestingClass.java
+++ src/test/java/GitPackage/TestingClass.java
@@ -2,6 +2,6 @@ package GitPackage;
 
 public class TestingClass {
     public static void main(String[] args) {
-        System.out.println("New changes in remote");
+        System.out.println("New changes in remotesd");
     }
 }
