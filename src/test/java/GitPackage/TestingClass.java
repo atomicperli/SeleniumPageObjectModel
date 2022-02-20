c78282fe7c3bcea1e0ff29a0c0992efc53dbd3a5
yperli <yperli@cisco.com>
2022-02-20 17:53:24 +0530

    Changes to TestingClass.java

--- src/test/java/GitPackage/TestingClass.java
+++ src/test/java/GitPackage/TestingClass.java
@@ -2,6 +2,6 @@ package GitPackage;
 
 public class TestingClass {
     public static void main(String[] args) {
-        System.out.println("New changes in remotesd");
+        System.out.println("New changes in remote");
     }
 }
